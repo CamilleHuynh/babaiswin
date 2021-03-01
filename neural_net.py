4a210c83cbc2687ddd8f01d24c8a772c444ca117
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2021-03-01 15:54:43 +0100

    Replace neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pygame as pg
 from stateHandler import step, printRules, simplify, isWinState
-from spritesheet import Spritesheet
 from copy import deepcopy
 import state
 
@@ -17,20 +15,20 @@ class DQN(nn.Module):
 
     def __init__(self, h, w, outputs):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(10, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(9, 16, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(16)
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
+        def conv2d_size_out(size, kernel_size = 3, stride=1, padding=1):
+            return (size + 2*padding- (kernel_size - 1) - 1) // stride + 1
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
+        linear_input_size = convw * convh * 16
         self.head = nn.Linear(linear_input_size, outputs)
 
     # Called with either one element to determine next action, or a batch
