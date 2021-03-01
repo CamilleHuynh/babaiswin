cd24cfbeb98c46b8990648d8880e6e5cd702b523
Katia JDL <katia.jodogne-del-litto@polytechnique.edu>
2021-03-01 15:53:29 +0100

    Essai

--- babaiswin.py
+++ babaiswin.py
@@ -25,15 +25,18 @@ background_color = (225, 225, 225)
 # Ce qui est est affiché à l'écran est 
 # la matrice map
 
-#Si changement, modifier aussi liste de états dans stateHandler (getStateList())
-map =   [[["no"], ["no"], ["no"], ["no"], ["no"]],
-         [["ft"], ["no"], ["wt"], ["no"], ["no"]],
-         [["bt"], ["is"], ["yt"], ["no"], ["no"]],
-         [["no"], ["bo"], ["ro"], ["ro"], ["no"]],
-         [["no"], ["is"], ["wo"], ["no"], ["no"]],
-         [["no"], ["no"], ["no"], ["no"], ["fo"]]]
+def reset():
+    #Si changement, modifier aussi liste de états dans stateHandler (getStateList())
+    map =   [[["no"], ["no"], ["no"], ["no"], ["no"]],
+             [["ft"], ["no"], ["wt"], ["no"], ["no"]],
+             [["bt"], ["is"], ["yt"], ["no"], ["no"]],
+             [["no"], ["bo"], ["ro"], ["ro"], ["no"]],
+             [["no"], ["is"], ["wo"], ["no"], ["no"]],
+             [["no"], ["no"], ["no"], ["no"], ["fo"]]]
+    
+    return map
 
-
+map = reset()
 
 state =[]
 for i in range(len(map[0])):
